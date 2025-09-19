9e689483b097180f4b718f04eddf10d56094de02
MRN7BAN <mrn7ban@bosch.com>
2025-09-19 19:58:23 +0530

    Initial Commit

---
+++ FixeTests/FixeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FixeTests.swift
+//  FixeTests
+//
+//  Created by MRN7BAN on 19/09/25.
+//
+
+import Testing
+@testable import Fixe
+
+struct FixeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
