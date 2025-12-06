9ae2e97aa3df8c00e63cb4f6ee6cbf110701d13e
Aravind Manikandan <rmaravind2002@gmail.com>
2025-12-06 16:25:02 +0530

    patch 2

---
+++ Fixe/Resources/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constans.swift
+//  Fixe
+//
+//  Created by MRN7BAN on 18/10/25.
+//
+import Foundation
+
+struct Constants {    
+    struct ServiceDataProvider {
+        static let applianceTypes = ["Refrigerators", "Dishwashers", "Washers", "Dryers"]
+        static let brands = ["Bosch", "Siemens"]
+        static let serviceTypes = ["General", "Repair"]
+        static let urgencyLevels = ["High", "Moderate", "Low"]
+    }
+}
